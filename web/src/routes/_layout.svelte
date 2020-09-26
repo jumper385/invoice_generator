df8fe4c88d740118e0f78572e380338a6640045d
Henry Chen <henrychen385@gmail.com>
2020-09-26 16:22:19 +0800

    added stashing and mobile views

--- web/src/routes/_layout.svelte
+++ web/src/routes/_layout.svelte
@@ -4,6 +4,12 @@
 		padding:64pt;
 		line-height:1;
 	}
+
+	@media only screen and (max-width:800px) {
+		main {
+			padding: 24pt;
+		}
+	}
 </style>
 
 <main>
--- web/src/routes/logging/index.svelte
+++ web/src/routes/logging/index.svelte
@@ -10,7 +10,9 @@
 
 <script>
   export let logs;
-  console.log(logs.length)
+  $: completed = logs.filter((x) => x.billed || x.stashed).splice(0, 6);
+  $: incomplete = logs.filter((x) => x.billed == false);
+
   import { dateString } from "../../helpers/datestring";
 
   let form_obj = {};
@@ -25,7 +27,7 @@
 
     if (res.status == 200) {
       logs = await (await fetch("logging.json")).json();
-      form_obj = {}
+      form_obj = {};
     }
   };
 
@@ -44,13 +46,31 @@
   };
 
   const billLog = async (id) => {
-    console.log(id);
     let res = await fetch("logging.json", {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ query: { _id: id }, delta: { billed: true } }),
+      body: JSON.stringify({
+        query: { _id: id },
+        delta: { billed: true, stashed: false },
+      }),
+    });
+    if (res.status == 200) {
+      logs = await (await fetch("logging.json")).json();
+    }
+  };
+
+  const stashLog = async (id) => {
+    let res = await fetch("logging.json", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: { _id: id },
+        delta: { stashed: true, billed: false },
+      }),
     });
     if (res.status == 200) {
       logs = await (await fetch("logging.json")).json();
@@ -58,14 +78,55 @@
   };
 </script>
 
-<div style='margin:16pt 0;'>
-    <a href='.'>Return Home...</a>
-</div>
+<style>
+  h3 {
+    text-align: center;
+    margin-bottom: 12pt;
+    font-weight: bold;
+  }
+  form {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    margin-top: 12pt;
+    grid-column-gap: 24pt;
+  }
+  .input_field {
+    display: grid;
+    grid-template-rows: 1fr1 1.5fr;
+  }
 
-<h3 style='margin-top:18pt'>Add New Task</h3>
-<form
-  style="display:grid; grid-template-columns: 1fr 1fr 1fr; margin-top:12pt;"
-  on:submit|preventDefault={logSubmission}>
+  .input_field label {
+    margin-bottom: 3pt;
+    font-size:0.75rem;
+    padding-left: 6pt;
+    text-transform: uppercase;
+    font-weight:bold;
+    color:rgba(0,0,0,.24);
+  }
+
+  .input_field input {
+    width: 100%;
+    margin-bottom:12pt;
+    display:flex;
+    align-items: center;
+    padding:6pt;
+    border-radius:2pt;
+    box-sizing:border-box;
+    border: rgba(0,0,0,.24) solid 1px;
+  }
+
+  @media only screen and (max-width:800px) {
+    form {
+      display:flex !important;
+      flex-direction: column;
+    }
+  }
+</style>
+
+<div style="margin:16pt 0;"><a href=".">Return Home...</a></div>
+
+<h3 style="margin-top:18pt">Add New Task</h3>
+<form on:submit|preventDefault={logSubmission} style='margin:0; width:100%; box-sizing:border-box;'>
   <div class="input_field">
     <label for="description">Task Description</label>
     <input
@@ -83,11 +144,44 @@
       name="hours"
       id="hours" />
   </div>
-  <input type="submit" />
+  <input type="submit" disabled={!form_obj.hours}/>
 </form>
 <br />
 
-<h3 style='margin-top:18pt'>Current Billable Tasks</h3>
+<h3 style="margin-top:18pt">Current Billable Tasks</h3>
+<div style="display:grid; grid-template-columns:1fr 3fr 1fr 1fr 1fr;">
+  <p style="font-weight:bold">Execution Date</p>
+  <p style="font-weight:bold">Description</p>
+  <p style="font-weight:bold">Hours</p>
+  <p style="font-weight:bold">Cost</p>
+  <p style="font-weight:bold">Actions</p>
+
+  {#if incomplete.length == 0}
+    <p style="grid-column: span 5; text-align:center">
+      Time for more bread! 🍞
+    </p>
+  {:else}
+    {#each incomplete as log}
+      <p>{dateString(new Date(log.timestamp))}</p>
+      <p>{log.description}</p>
+      <p>{log.hours}</p>
+      <p>{log.hours * log.rate}</p>
+      <div style="display:flex; align-items:center">
+        <button
+          on:click={() => billLog(log._id)}
+          disabled={log.billed}>bill</button>
+        <button
+          on:click={() => deleteLog(log._id)}
+          disabled={log.billed}>delete</button>
+        <button
+          on:click={() => stashLog(log._id)}
+          disabled={log.billed || log.stashed}>stash</button>
+      </div>
+    {/each}
+  {/if}
+</div>
+
+<h3 style="margin-top:18pt">Recently Finalized Tasks</h3>
 <div style="display:grid; grid-template-columns:1fr 3fr 1fr 1fr 1fr;">
   <p style="font-weight:bold">Execution Date</p>
   <p style="font-weight:bold">Description</p>
@@ -96,18 +190,26 @@
   <p style="font-weight:bold">Actions</p>
 
   {#if logs.length == 0}
-  <p style='grid-column: span 5; text-align:center'>Time for more bread! 🍞</p>
+    <p style="grid-column: span 5; text-align:center">
+      Time for more bread! 🍞
+    </p>
   {:else}
-    {#each logs as log}
-        <p>{dateString(new Date(log.timestamp))}</p>
-        <p>{log.description}</p>
-        <p>{log.hours}</p>
-        <p>{log.hours * log.rate}</p>
-        <div style="display:flex; align-items:center">
-        <button on:click={() => billLog(log._id)}>billed</button>
-        <button on:click={() => deleteLog(log._id)}>delete</button>
-        </div>
+    {#each completed as log}
+      <p>{dateString(new Date(log.timestamp))}</p>
+      <p>{log.description}</p>
+      <p>{log.hours}</p>
+      <p>{log.hours * log.rate}</p>
+      <div style="display:flex; align-items:center">
+        <button
+          on:click={() => billLog(log._id)}
+          disabled={log.billed}>bill</button>
+        <button
+          on:click={() => deleteLog(log._id)}
+          disabled={log.billed}>delete</button>
+        <button
+          on:click={() => stashLog(log._id)}
+          disabled={log.billed || log.stashed}>stash</button>
+      </div>
     {/each}
   {/if}
-
 </div>
