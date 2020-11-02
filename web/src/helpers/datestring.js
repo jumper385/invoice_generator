3ace55265709112da1df8b4dc2588af9ce975d6b
jumper385 <henrychen385@gmail.com>
2020-11-02 11:41:02 +0800

    capitalized all the months

--- web/src/helpers/datestring.js
+++ web/src/helpers/datestring.js
@@ -1,4 +1,4 @@
 export const dateString = (date) => {
-    let monthLUT = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sept', 'oct', 'nov', 'dec'];
+    let monthLUT = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
     return `${date.getDate()} ${monthLUT[date.getMonth()]} ${date.getFullYear()}`
 }
