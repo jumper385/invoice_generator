a8c0cb9f68551f882deec3cf42d328c13e9a8266
jumper385 <henrychen385@gmail.com>
2020-11-02 11:41:38 +0800

    added API route

---
+++ web/src/routes/api/index.js
@@ -0,0 +1,20 @@
+import * as db from '../../dbhelpers/mongoosehelpers';
+import { WorkLog } from '../../dbhelpers/schemas';
+
+export const get = async (req, res) => {
+	res.json(await db.queryCollection(WorkLog));
+}
+
+export const post = async (req, res) => {
+	res.json(await db.postCollection(WorkLog, req.body));
+}
+
+export const put = async (req, res) => {
+    let { query, delta } = req.body;
+    awaitdb.editCollection(WorkLog, query, delta)
+    res.json(await db.queryCollection(WorkLog, query));
+}
+
+export const del = async (req, res) => {
+	res.json(await db.deleteCollection(WorkLog, req.body));
+}
