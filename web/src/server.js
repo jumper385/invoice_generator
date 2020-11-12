3d2e3f7c14d6377036408c212cb8b4a53322c49e
jumper385 <henrychen385@gmail.com>
2020-11-12 17:17:06 +0800

    added cors

--- web/src/server.js
+++ web/src/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 import bodyParser from 'body-parser';
+import cors from 'cors'
 
 const app = express();
 
@@ -11,6 +12,7 @@ const dev = NODE_ENV === 'development';
 
 app.use(compression({ threshold: 0 }));
 app.use(sirv('static', { dev }));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(sapper.middleware());
